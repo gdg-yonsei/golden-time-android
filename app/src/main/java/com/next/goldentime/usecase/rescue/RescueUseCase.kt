3c3509afb6ce4f1d0b752c0e344ac34c18786254
namooplus <namoo.developer@gmail.com>
2023-03-11 00:55:58 +0900

    feat: get manual from rescue usecase

--- app/src/main/java/com/next/goldentime/usecase/rescue/RescueUseCase.kt
+++ app/src/main/java/com/next/goldentime/usecase/rescue/RescueUseCase.kt
@@ -1,10 +1,16 @@
 package com.next.goldentime.usecase.rescue
 
+import com.next.goldentime.repository.disease.DiseaseRepository
 import com.next.goldentime.repository.sos.Location
 import com.next.goldentime.repository.sos.SOSRepository
 
-class RescueUseCase(private val sosRepository: SOSRepository, private val sosId: Int) {
+class RescueUseCase(
+    private val sosRepository: SOSRepository,
+    private val diseaseRepository: DiseaseRepository,
+    private val sosId: Int
+) {
     fun getSOS() = sosRepository.getSOS(sosId)
+    fun getManual(diseaseId: Int) = diseaseRepository.getManual(diseaseId)
     suspend fun acceptSOS() = sosRepository.acceptSOS(sosId)
     suspend fun postLocation(location: Location) = sosRepository.postLocation(sosId, location)
     suspend fun markAsArrived() = sosRepository.markAsArrived(sosId)
