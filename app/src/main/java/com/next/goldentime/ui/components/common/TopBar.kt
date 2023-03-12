ae3d7092353239abc04080640639fd58fe05ea25
namooplus <namoo.developer@gmail.com>
2023-03-12 16:07:46 +0900

    feat: add top bar and with top bar component

---
+++ app/src/main/java/com/next/goldentime/ui/components/common/TopBar.kt
@@ -0,0 +1,62 @@
+package com.next.goldentime.ui.components.common
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+
+data class TopBarIcon(val icon: ImageVector, val onClick: () -> Unit)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String,
+    left: TopBarIcon? = null,
+    right: TopBarIcon? = null,
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            left?.let {
+                IconButton(onClick = it.onClick) {
+                    Icon(imageVector = it.icon, contentDescription = null)
+                }
+            }
+        },
+        actions = {
+            right?.let {
+                IconButton(onClick = it.onClick) {
+                    Icon(imageVector = it.icon, contentDescription = null)
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun TopBarPreview1() {
+    TopBar("Title")
+}
+
+@Preview
+@Composable
+private fun TopBarPreview2() {
+    TopBar(
+        "Title",
+        left = TopBarIcon(Icons.Default.ArrowBack) {},
+    )
+}
+
+@Preview
+@Composable
+private fun TopBarPreview3() {
+    TopBar(
+        "Title",
+        left = TopBarIcon(Icons.Default.ArrowBack) {},
+        right = TopBarIcon(Icons.Default.Settings) {},
+    )
+}
---
+++ app/src/main/java/com/next/goldentime/ui/components/common/WithTopBar.kt
@@ -0,0 +1,40 @@
+package com.next.goldentime.ui.components.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WithTopBar(topBar: @Composable () -> Unit, content: @Composable () -> Unit) {
+    Column {
+        topBar()
+        Box(modifier = Modifier.weight(1f)) {
+            content()
+        }
+    }
+}
+
+@Preview(device = Devices.PHONE)
+@Composable
+fun WithTopBarPreview1() {
+    WithTopBar(topBar = { TopBar("Title") }) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Hello")
+        }
+    }
+}
